6f169150412aa7cb40917aa135f51888a643bb3c
Felix Bredl <Elektro-Bredl@Outlook.de>
2025-12-07 09:09:33 +0100

    Website wieder in Wartungsmodus versetzt

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import OnePage from './pages/OnePage';
-import Impressum from './pages/Impressum';
-import Datenschutz from './pages/Datenschutz';
-import Footer from './components/Footer';
-import CookieBanner from './components/CookieBanner';
+import Maintenance from './pages/Maintenance';
 
 function App() {
+  // Wartungsmodus aktiviert - Website ist offline
+  return <Maintenance />;
+  
+  /* Original-Code für später (wenn Website wieder online):
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import Navbar from './components/Navbar';
+  import OnePage from './pages/OnePage';
+  import Impressum from './pages/Impressum';
+  import Datenschutz from './pages/Datenschutz';
+  import Footer from './components/Footer';
+  import CookieBanner from './components/CookieBanner';
+
   return (
     <BrowserRouter>
-    <div className="min-h-screen bg-background flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
+      <div className="min-h-screen bg-background flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<OnePage />} />
             <Route path="/impressum" element={<Impressum />} />
             <Route path="/datenschutz" element={<Datenschutz />} />
           </Routes>
-      </main>
-      <Footer />
-      <CookieBanner />
-    </div>
+        </main>
+        <Footer />
+        <CookieBanner />
+      </div>
     </BrowserRouter>
   );
+  */
 }
 
 export default App;
